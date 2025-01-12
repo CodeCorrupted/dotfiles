a9e189cd2391d68188302689284b524820eccd5e
Ivan Gomez <noob.programer.java@gmail.com>
2025-01-12 18:54:58 -0300

    "Add some comments to autostart and modify some bindings"

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,18 +1,38 @@
 #!/bin/sh
 
+# This function only executes a program if it's not already running
 run() {
   if ! pgrep -f "$1"; then
     "$@" &
   fi
 }
 
+# Set keyboard layout to es-latam
 run setxkbmap latam
+
+# Start picom as daemon
 run picom -b
+
+# Start applet of network manager connman
 run connman-gtk
+
+# Start parcellite, a clipboard manager
 run parcellite
+
+# Start redshift, a program to control the temperature of your screen
 run redshift-gtk
-run udiskie -ants
+
+# Start the icon of an automonter of removable media
+run udiskie -as
+
+# Start a notification daemon
 run dunst
+
+# Start mpd with its config file
 run mpd ~/.config/mpd/mpd.conf
+
+# Set wallpapper
 run ~/.fehbg
+
+# Start bluetooth applet
 run blueman-applet
