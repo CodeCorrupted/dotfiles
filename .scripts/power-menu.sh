00cdbfe6048255361a8262b8ddddb2cc5b1f0e86
Ivan Gomez <noob.programer.java@gmail.com>
2025-01-17 11:08:35 -0300

    "Add script of power menu to logout/power off/reboot/lock system"

---
+++ .scripts/power-menu.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# Current Theme
+dir="$HOME/.config/rofi/themes/"
+theme='power-menu'
+
+# CMDs
+uptime="$(uptime -p | sed -e 's/up //g')"
+host=$(hostname)
+
+# Options
+shutdown=' '
+reboot=' '
+lock=' '
+logout=' '
+yes=' '
+no=' '
+
+# Rofi CMD
+rofi_cmd() {
+  rofi -dmenu \
+    -p "Uptime: $uptime" \
+    -mesg "Uptime: $uptime" \
+    -theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+  rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+    -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+    -theme-str 'listview {columns: 2; lines: 1;}' \
+    -theme-str 'element-text {horizontal-align: 0.5;}' \
+    -theme-str 'textbox {horizontal-align: 0.5;}' \
+    -dmenu \
+    -p 'Confirmation' \
+    -mesg 'Are you Sure?' \
+    -theme ${dir}/${theme}.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+  echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+  echo -e "$lock\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+  selected="$(confirm_exit)"
+  if [[ "$selected" == "$yes" ]]; then
+    if [[ $1 == '--shutdown' ]]; then
+      loginctl poweroff -i
+    elif [[ $1 == '--reboot' ]]; then
+      loginctl reboot
+    elif [[ $1 == '--logout' ]]; then
+      if [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+        bspc quit
+      elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+        i3-msg exit
+      fi
+    fi
+  else
+    exit 0
+  fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+$shutdown)
+  run_cmd --shutdown
+  ;;
+$reboot)
+  run_cmd --reboot
+  ;;
+$lock)
+  if [[ -x '/usr/bin/betterlockscreen' ]]; then
+    betterlockscreen -l
+  elif [[ -x '/usr/bin/i3lock' ]]; then
+    i3lock -i ~/Images/Wallpapers/c59db64e-9d28-4839-b809-f7210404be01.png
+  fi
+  ;;
+$logout)
+  run_cmd --logout
+  ;;
+esac
